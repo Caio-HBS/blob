01529266db4ddb2788c051cacbfa3a98e426f4d5
Caio Bianchi <caiohbs23@gmail.com>
2024-01-10 16:44:34 -0300

    updates in models and index.html

--- src/blob/models.py
+++ src/blob/models.py
@@ -7,7 +7,15 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     """
-    TODO: documentation.
+    Represents a Post.
+
+    Attributes:
+        owner: the `User` to which the Post belongs;
+        title: the title of the Post;
+        image: the image for the Post (not required);
+        text: the text for the Post;
+        slug: the slug for the Post (provided automatically);
+        time: the time the Post was made (provided automatically).
     """
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -22,7 +30,7 @@ class Post(models.Model):
 
     def save(self, **kwargs):
         """
-        TODO: documentation.
+        Provides a title based on the text for the Post, as well as a unique slug.
         """
         if not self.title:
             try:
@@ -40,7 +48,13 @@ class Post(models.Model):
 
 class Comments(models.Model):
     """
-    TODO: documentation.
+    A Comment instance for the Posts.
+
+    Attributes:
+        owner: the `User` that left the comment;
+        related_post: the Post to which the comment was left;
+        text: the text for the Comment;
+        time: the time the Comment was made (provided automatically).
     """
 
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
